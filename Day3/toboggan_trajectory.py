fd092e5902548b4be2c85440107cd091b11ae750
Daniel Golden <35408027+daniel-golden@users.noreply.github.com>
2020-12-02 23:12:40 -0600

    Day 3 Advent of Code

---
+++ Day3/toboggan_trajectory.py
@@ -0,0 +1,24 @@
+terrain = []
+with open('input.txt', 'r') as file:
+    terrain = file.read().splitlines()
+
+
+
+slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+counts = []
+for x, y in slopes:
+    tree_count = 0
+    position_x = 0
+    position_y = 0
+    while position_y < len(terrain):
+        if terrain[position_y][position_x] == '#':
+            tree_count += 1
+        position_x = (position_x + x) % len(terrain[0])
+        position_y += y
+
+    counts.append(tree_count)
+total = 1
+for count in counts:
+    total *= count
+
+print(total)
